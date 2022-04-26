2676af4ba8321cc3fda55a59c08ccff6a80613ce
allisonvinicius <allisonvinicius63@gmail.com>
2022-04-25 21:32:20 -0400

    context, and reducer state initial in component application

--- barberApp/App.js
+++ barberApp/App.js
@@ -1,11 +1,14 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import UserContextProvider from './src/contexts/UserContext';
 import MainStack from './src/stacks/MainStack';
 
 export default () => {
   return (
-    <NavigationContainer>
-      <MainStack />
-    </NavigationContainer>
+    <UserContextProvider>
+      <NavigationContainer>
+        <MainStack />
+      </NavigationContainer>
+    </UserContextProvider>
   );
 };
