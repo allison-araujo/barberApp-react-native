bc31d2190a9b112dbd5cf82fbf265204dfda5bb4
allisonvinicius <allisonvinicius63@gmail.com>
2022-05-01 21:24:02 -0400

    add icons in tabBar

--- barberApp/src/components/CustomTabBar/CustomTabBar.js
+++ barberApp/src/components/CustomTabBar/CustomTabBar.js
@@ -1,4 +1,10 @@
+import React from 'react';
 import styled from 'styled-components';
+import AccountIcon from '../../assets/account.svg';
+import FavoritiesIcon from '../../assets/favorities.svg';
+import HomeIcon from '../../assets/home.svg';
+import SearchIcon from '../../assets/search.svg';
+import TodayIcon from '../../assets/today.svg';
 
 const TabArea = styled.View`
   height: 60px;
@@ -6,6 +12,30 @@ const TabArea = styled.View`
   flex-direction: row;
 `;
 
+const TabItem = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
 export default () => {
-  return <TabArea></TabArea>;
+  return (
+    <TabArea>
+      <TabItem>
+        <HomeIcon width="24" height="24" fill="#FFFF" />
+      </TabItem>
+      <TabItem>
+        <SearchIcon width="24" height="24" fill="#FFFF" />
+      </TabItem>
+      <TabItem>
+        <TodayIcon width="24" height="24" fill="#FFFF" />
+      </TabItem>
+      <TabItem>
+        <FavoritiesIcon width="24" height="24" fill="#FFFF" />
+      </TabItem>
+      <TabItem>
+        <AccountIcon width="24" height="24" fill="#FFFF" />
+      </TabItem>
+    </TabArea>
+  );
 };
