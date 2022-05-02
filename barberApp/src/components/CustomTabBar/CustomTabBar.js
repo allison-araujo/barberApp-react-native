6d1dc1d8c2820de1ef229c579525653dd0b0951a
allisonvinicius <allisonvinicius63@gmail.com>
2022-05-01 21:09:55 -0400

    component TabBar

---
+++ barberApp/src/components/CustomTabBar/CustomTabBar.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const TabArea = styled.View`
+  height: 60px;
+  background-color: #4eadbe;
+  flex-direction: row;
+`;
+
+export default () => {
+  return <TabArea></TabArea>;
+};
