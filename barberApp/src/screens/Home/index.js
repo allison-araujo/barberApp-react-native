68c50073e63411cff39a11bcdba9e9defa9908a4
allisonvinicius <allisonvinicius63@gmail.com>
2022-05-05 22:58:56 -0400

    create style component

--- barberApp/src/screens/Home/index.js
+++ barberApp/src/screens/Home/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import React, {useState} from 'react';
 import MyLocationIcon from '../../assets/my_location.svg';
 import SearchIcon from '../../assets/search.svg';
 import {
@@ -13,17 +14,25 @@ import {
 } from './styles';
 
 export default () => {
+  const navigation = useNavigation();
+  const [locationText, setLocationText] = useState();
+
   return (
     <Container>
       <Scroller>
         <HeaderArea>
           <HeaderTitle numberOfLines={2}>Encontre sua barbearis</HeaderTitle>
-          <SearchButton>
+          <SearchButton onPress={() => navigation.navigate('Search')}>
             <SearchIcon width="26" height="26" fill="#FFF" />
           </SearchButton>
         </HeaderArea>
         <LocationArea>
-          <LocationInput />
+          <LocationInput
+            placeholder="Onde esta"
+            placeholderTextColor="#FFF"
+            value={locationText}
+            onChangeText={t => setLocationText(t)}
+          />
           <LocationFinder>
             <MyLocationIcon width="24" height="24" fill="#FFF" />
           </LocationFinder>
