1bface39bd4787295ae220013ff964c66d3a9d9d
allisonvinicius <allisonvinicius63@gmail.com>
2022-05-09 22:16:18 -0400

    update styles component

--- barberApp/src/components/Stars/Stars.js
+++ barberApp/src/components/Stars/Stars.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import StarEmpty from '../../assets/star_empty.svg';
+import StarFull from '../../assets/star_full.svg';
 import StarHalf from '../../assets/star_half.svg';
 
-
 const StarArea = styled.View`
-flex-direction: row;
+  flex-direction: row;
 `;
 
-export default ({stars,showAssess}) => {
-  let valueSTars = [0,0,0,0,0];
+export default ({stars, showAssess}) => {
+  let valueSTars = [0, 0, 0, 0, 0];
 
-  
   return (
-  <StarArea>
-     {valueSTars.map((p, quantity) => {
-       <StarView key={quantity}>
-         { p === 0 && <StarEmpty width="18" height="18" fill="#FF9200" />}
-         { p === 1 && <StarHalf width="18" height="18" fill="#FF9200" />}
-         { p ===  2 && <StarFull width="18" height="18" fill="#FF9200" />}
-         
-       </StarView>
-     })}
-     </StarArea>
-  )
-
+    <StarArea>
+      {valueSTars.map((p, quantity) => (
+        <StarView key={quantity}>
+          {p === 0 && <StarEmpty width="18" height="18" fill="#FF9200" />}
+          {p === 1 && <StarHalf width="18" height="18" fill="#FF9200" />}
+          {p === 2 && <StarFull width="18" height="18" fill="#FF9200" />}
+        </StarView>
+      ))}
+    </StarArea>
+  );
+};
