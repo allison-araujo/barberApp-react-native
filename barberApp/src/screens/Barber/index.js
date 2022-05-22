5bf70f00191fa4b5d9cacffcadce73445d74f705
allisonvinicius <allisonvinicius63@gmail.com>
2022-05-22 19:15:43 -0400

    effect getBArber

--- barberApp/src/screens/Barber/index.js
+++ barberApp/src/screens/Barber/index.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { Text } from 'react-native';
+import Api from '../../Api';
+import { Container } from './styles';
 
 export default () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const [userInfo, setUserInfor] = useState({
+    id: route.params.id,
+    avatar: route.params.avatar,
+    name: route.params.name,
+    stars: route.params.start,
+  });
+
+  useEffect(() => {
+    const getBarberInfo = async () => {
+      let json = await Api.getBarberListId(userInfo.id);
+      if(json.error == ''){
+
+      }else{
+        alert('error'+ json.error).;
+      }
+    };
+    getBarberInfo();
+  }, []);
+
   return (
     <Container>
-      <Text>Barber</Text>
+      <Text>Barber: {userInfo.name} </Text>
     </Container>
   );
 };
