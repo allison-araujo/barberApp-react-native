b5563913a204a333f9546f7df7495449dd1ee6b1
allisonvinicius <allisonvinicius63@gmail.com>
2022-05-22 19:23:33 -0400

    hotfix: update infos

--- barberApp/src/screens/Barber/index.js
+++ barberApp/src/screens/Barber/index.js
@@ -1,12 +1,13 @@
-import { useNavigation, useRoute } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
-import { Text } from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
+import {Text} from 'react-native';
 import Api from '../../Api';
-import { Container } from './styles';
+import {Container} from './styles';
 
 export default () => {
   const navigation = useNavigation();
   const route = useRoute();
+  const [loading, setLoading] = useState(false);
 
   const [userInfo, setUserInfor] = useState({
     id: route.params.id,
@@ -17,15 +18,17 @@ export default () => {
 
   useEffect(() => {
     const getBarberInfo = async () => {
+      setLoading(true);
       let json = await Api.getBarberListId(userInfo.id);
-      if(json.error == ''){
-
-      }else{
-        alert('error'+ json.error).;
+      if (json.error === '') {
+        setUserInfor(json.data);
+      } else {
+        alert('error' + json.error);
       }
+      setLoading(false);
     };
     getBarberInfo();
-  }, []);
+  }, [userInfo.id]);
 
   return (
     <Container>
