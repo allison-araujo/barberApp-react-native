df8b8a833b56acd5e118ec6e0f492d959e2e6ef4
allisonvinicius <allisonvinicius63@gmail.com>
2022-07-06 22:30:37 -0400

    fix: include styles and button modal

--- barberApp/src/components/BarberModal/BarberModal.js
+++ barberApp/src/components/BarberModal/BarberModal.js
@@ -2,15 +2,35 @@ import styled from 'styled-components/native';
 
 const Modal = styled.Modal``;
 
-const ModalArea = styled.View``;
+const ModalArea = styled.View`
+  flex: 1;
+  background-color: rgba(0, 0, 0, 0.5);
+  justify-content: flex-end;
+`;
 
-const ModalBody = styled.View``;
+const ModalBody = styled.View`
+  background-color: #83d6e3;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  min-height: 300px;
+  padding: 10px 20px 40px 20px;
+`;
+
+const CloseButton = styled.TouchableOpacity`
+  width: 40px;
+  height: 40px;
+`;
 
 export default ({show, setShow, user, service}) => {
+  const handleCloseButton = () => {
+    setShow(false);
+  };
   return (
     <Modal transparent={true} visible={show} animationType="slide">
       <ModalArea>
-        <ModalBody></ModalBody>
+        <ModalBody>
+          <CloseButton onPress={handleCloseButton}></CloseButton>
+        </ModalBody>
       </ModalArea>
     </Modal>
   );
